604dd5fdcdf4eb244f434ab30cd0762c4558795e
keepsmile <109835582+mathlearner-hard@users.noreply.github.com>
2024-12-08 11:10:43 +0800

    2024.8.12
    利用myst_parser包解决不支持markdown语法问题

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 #extensions = []
-extensions = ['recommonmark']
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
